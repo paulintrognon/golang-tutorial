63f8a036570a309f7a1fbbd35d0d5a438eb7fdaa
Paulin Trognon <paulin.trognon@gmail.com>
2019-12-15 16:54:34 +0100

    learn variables

--- src/github.com/paulintrognon/golang-tutorial/Main.go
+++ src/github.com/paulintrognon/golang-tutorial/Main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello Margot")
+	var (
+		age  = 29
+		name = "Paulin"
+	)
+
+	fmt.Println("Je suis " + name + ", j'ai " + strconv.Itoa(age) + " ans et je suis " + majorityStatus(age))
+}
+
+func majorityStatus(age int) string {
+	if age >= 18 {
+		return "majeur"
+	}
+	return "minor"
 }
