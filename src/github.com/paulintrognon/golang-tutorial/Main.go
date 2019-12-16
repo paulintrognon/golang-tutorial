77c0021a6757d807127df9c7d28c9f99401340ee
Paulin Trognon <paulin.trognon@gmail.com>
2019-12-16 09:50:03 +0100

    add const

--- src/github.com/paulintrognon/golang-tutorial/Main.go
+++ src/github.com/paulintrognon/golang-tutorial/Main.go
@@ -5,18 +5,30 @@ import (
 	"strconv"
 )
 
+const (
+	isMale = iota + 1
+	isFemale
+)
+
 func main() {
 	var (
 		age  = 29
 		name = "Paulin"
+		sex  = isMale
 	)
 
-	fmt.Println("Je suis " + name + ", j'ai " + strconv.Itoa(age) + " ans et je suis " + majorityStatus(age))
+	fmt.Println("Je suis " + name + ", j'ai " + strconv.Itoa(age) + " (je suis " + majorityStatus(age) + ")")
+
+	if sex == isMale {
+		fmt.Println("Je suis un homme")
+	} else {
+		fmt.Println("Je suis une femme")
+	}
 }
 
 func majorityStatus(age int) string {
 	if age >= 18 {
 		return "majeur"
 	}
-	return "minor"
+	return "mineur"
 }
